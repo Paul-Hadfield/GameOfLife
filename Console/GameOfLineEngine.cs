53008c848ee7f69ff75edad2609f94bbc4f309a1
Paul Hadfield <paulhadfield@gmail.com>
2018-05-17 23:47:46 +0100

    Moved remaining code out of main

---
+++ Console/GameOfLineEngine.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using GameOfLife.Counters;
+using GameOfLife.Outputs;
+using GameOfLife.Rules;
+
+namespace GameOfLife
+{
+    public class GameOfLineEngine
+    {
+        private readonly IDefineANeigbourCounter counter;
+        private readonly IDefineARulesEngine rulesEngine;
+        private readonly IDefineAnOutputter output;
+
+        public GameOfLineEngine(IDefineANeigbourCounter counter, IDefineARulesEngine rulesEngine, IDefineAnOutputter output)
+        {
+            this.counter = counter;
+            this.rulesEngine = rulesEngine;
+            this.output = output;
+        }
+
+        public void Process(bool[,] grid)
+        {
+            while (true)
+            {
+                output.Write(grid);
+                grid = Process(this.counter, this.rulesEngine, grid);
+                Console.ReadKey();
+            }
+        }
+
+        private static bool[,] Process(IDefineANeigbourCounter counter, IDefineARulesEngine rulesEngine, bool[,] grid)
+        {
+            var newGrid = new bool[grid.GetLength(0), grid.GetLength(1)];
+
+            for (var x = 0; x < grid.GetLength(0); x++)
+            {
+                for (var y = 0; y < grid.GetLength(1); y++)
+                {
+                    var liveNeighbours = counter.Count(grid, x, y);
+                    newGrid[x, y] = rulesEngine.DetermineNewState(grid[x, y], liveNeighbours);
+                }
+            }
+
+            return newGrid;
+        }
+
+    }
+}
--- Console/Program.cs
+++ Console/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System;
 using GameOfLife.Counters;
 using GameOfLife.Outputs;
 using GameOfLife.Rules;
@@ -12,32 +11,14 @@ namespace GameOfLife
         static void Main(string[] args)
         {
             var grid = new BlinkerSeeder().Seed();
-            var output = new StreamingConsoleOutput();
-            var counter = new LiveNeighbourCounter();
-            var rulesEngine = new RulesEngine();
+            
+            var game = new GameOfLineEngine(
+                                new LiveNeighbourCounter(),
+                                new RulesEngine(),
+                                new StreamingConsoleOutput());
 
-            while (true)
-            {
-                output.Write(grid);
-                grid = Process(counter, rulesEngine, grid);
-                Console.ReadKey();
-            }
+            game.Process(grid);
         }
 
-        private static bool[,] Process(IDefineANeigbourCounter counter, RulesEngine rulesEngine, bool[,] grid)
-        {
-            var newGrid = new bool[grid.GetLength(0),grid.GetLength(1)];
-
-            for (var x = 0; x < grid.GetLength(0); x++)
-            {
-                for (var y = 0; y < grid.GetLength(1); y++)
-                {
-                    var liveNeighbours = counter.Count(grid, x,y);
-                    newGrid[x, y] = rulesEngine.DetermineNewState(grid[x, y], liveNeighbours);
-                }
-            }
-
-            return newGrid;
-        }
     }
 }
