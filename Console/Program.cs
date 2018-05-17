4dd1b769c144d8b23f329e91c7a48e49d8ae9b27
Paul Hadfield <paulhadfield@gmail.com>
2018-05-17 19:03:37 +0100

    Getting neighbours (with bug)

--- Console/Program.cs
+++ Console/Program.cs
@@ -55,13 +55,58 @@ namespace GameOfLife
             return false;
         }
 
-        private static int GetCountOfLiveNeighbours(bool[,] grid, int i, int i1)
+        private static int GetCountOfLiveNeighbours(bool[,] grid, int xPos, int yPos)
         {
-            return 0;
+            var minX = xPos - 1;
+            var maxX = xPos + 1;
+            var minY = yPos - 1;
+            var maxY = yPos + 1;
+
+            if (minX < 0)
+            {
+                minX = 0;
+            }
+
+            if (maxX >= grid.GetLength(0))
+            {
+                maxX = grid.GetLength(0) - 1;
+            }
+
+            if (minY < 0)
+            {
+                minY = 0;
+            }
+
+            if (maxY >= grid.GetLength(1))
+            {
+                maxY = grid.GetLength(1) - 1;
+            }
+
+            var liveNeighbours = 0;
+
+            for (var x = minX; x <= maxX; x++)
+            {
+                for (var y = minY; y <= maxY; y++)
+                {
+                    if (x == xPos && y == yPos)
+                    {
+                        continue;
+                    }
+
+                    if (grid[x, y])
+                    {
+                        liveNeighbours++;
+                    }
+                }
+            }
+
+            return liveNeighbours;
         }
 
         private static void Output(bool[,] grid)
         {
+
+            Console.WriteLine("Next Cycle>>>");
             for (var x = 0; x < grid.GetLength(0); x++)
             {
                 for (var y = 0; y < grid.GetLength(1); y++)
