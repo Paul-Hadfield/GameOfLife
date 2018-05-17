b6228935e71f50a15fe664d97d4e7f70ad504a41
Paul Hadfield <paulhadfield@gmail.com>
2018-05-17 21:49:37 +0100

    Added more rules

---
+++ Console/Rules/Rule2.cs
@@ -0,0 +1,15 @@
+﻿namespace GameOfLife.Rules
+{
+    public class Rule2 : IDefineARule
+    {
+        public bool IsMatched(bool alive, int liveNeighbours)
+        {
+            if (alive  && (liveNeighbours == 2 || liveNeighbours == 3))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Console/Rules/Rule3.cs
@@ -0,0 +1,15 @@
+﻿namespace GameOfLife.Rules
+{
+    public class Rule3 : IDefineARule
+    {
+        public bool IsMatched(bool alive, int liveNeighbours)
+        {
+            if (alive && liveNeighbours > 3)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
