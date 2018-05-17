8b309201034b373ed6805e3f3cfba992bfa1682e
Paul Hadfield <paulhadfield@gmail.com>
2018-05-17 18:44:18 +0100

    Start population

--- Console/Program.cs
+++ Console/Program.cs
@@ -10,6 +10,13 @@ namespace Console
     {
         static void Main(string[] args)
         {
+            var grid = Blinker();
+        }
+
+        private static bool[,] Blinker()
+        {
+            var grid = new bool[5, 5];
+            return grid;
         }
     }
 }
