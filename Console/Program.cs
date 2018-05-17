0f95d3a441d22ea7a5515c60cd896fb4a167af51
Paul Hadfield <paulhadfield@gmail.com>
2018-05-17 22:41:28 +0100

    Injecting instances

--- Console/Program.cs
+++ Console/Program.cs
@@ -13,16 +13,18 @@ namespace GameOfLife
         {
             var grid = new BlinkerSeeder().Seed();
             var output = new StreamingConsoleOutput();
+            var counter = new LiveNeighbourCounter();
+            var rulesEngine = new RulesEngine();
 
             while (true)
             {
                 output.Write(grid);
-                grid = Process(grid);
+                grid = Process(counter, rulesEngine, grid);
                 Console.ReadKey();
             }
         }
 
-        private static bool[,] Process(bool[,] grid)
+        private static bool[,] Process(IDefineANeigbourCounter counter, RulesEngine rulesEngine, bool[,] grid)
         {
             var newGrid = new bool[grid.GetLength(0),grid.GetLength(1)];
 
@@ -30,8 +32,8 @@ namespace GameOfLife
             {
                 for (var y = 0; y < grid.GetLength(1); y++)
                 {
-                    var liveNeighbours = new LiveNeighbourCounter().Count(grid, x,y);
-                    newGrid[x, y] = new RulesEngine().DetermineNewState(grid[x, y], liveNeighbours);
+                    var liveNeighbours = counter.Count(grid, x,y);
+                    newGrid[x, y] = rulesEngine.DetermineNewState(grid[x, y], liveNeighbours);
                 }
             }
 
